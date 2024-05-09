87c1eb650cd20bb1e7fa3f3f0caefa78f5012098
“Guada” <guadalupearroferia@gmail.com>
2024-05-09 10:40:12 +1000

    ADD style with Sass

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './Css/main.css';
 import App from './App';
+import "./scss/index.scss"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
-
 );
