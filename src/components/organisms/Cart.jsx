e81054cf146eed9dcc634aede23f569bccb6ab26
“Guada” <guadalupearroferia@gmail.com>
2024-05-10 16:01:29 +1000

    CORRECT cart logic

--- src/components/organisms/Cart.jsx
+++ src/components/organisms/Cart.jsx
@@ -1,22 +1,21 @@
-import React, { useContext } from 'react'
-import Icon from '../molecules/Icon'
-import { CartContext } from '../../context/CartContext'
+import React, { useContext } from "react";
+import Icon from "../molecules/Icon";
+import { CartContext } from "../../context/CartContext";
 
 const Cart = () => {
-    const {toysCart} = useContext(CartContext)
+  const { toysCart } = useContext(CartContext);
   return (
-    <div className='navbar__cart-container'>
-      <button className='navbar__cart-btn'>
-          <Icon className='navbar__cart-span'/>
+    <div className="navbar__cart-container">
+      <button className="navbar__cart-btn">
+        <Icon className="navbar__cart-span" />
       </button>
-      <div className='navbar__cart-badge'>
-          {toysCart.reduce(
-            (acc, toy)=> acc + toy.quantity,
-            0
-          )}
-      </div>
-    </ div>
-  )
-}
+      {toysCart.length > 0 &&
+        <div className="navbar__cart-badge">
+          {toysCart.reduce((acc, toy) => acc + toy.quantity, 0)}
+        </div>
+      }
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
