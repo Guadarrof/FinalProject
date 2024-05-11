ed95cdb7ce9ccc97b911e5410b7cfda8940f4ea1
“Guada” <guadalupearroferia@gmail.com>
2024-05-11 10:26:44 +1000

    ADD link to card content

--- src/components/molecules/CardSm.jsx
+++ src/components/molecules/CardSm.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React from "react";
 import Counter from "./Counter";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const CardSm = ({routePrefix, urlImg, imgDescription, price, title, description, id}) => {
+const CardSm = ({
+  routePrefix,
+  urlImg,
+  imgDescription,
+  price,
+  title,
+  description,
+  id,
+}) => {
   return (
     <div className="card card-m" id="product1">
-        <img src={urlImg} alt={imgDescription} className="card_img"/>
-        <div className="card_text">
-            <p className="card__price">{price}</p>
-            <h3 className="card__h3">{title}</h3>
-            <p className="card__p">{description}</p>
-            <Link to={`${routePrefix}/${id}`} className="card__a">Mas informacion</Link>
-        </div>
-        <Counter id={id}/>
+      <img src={urlImg} alt={imgDescription} className="card_img" />
+      <Link to={`${routePrefix}/${id}`} className="card_text">
+        <p className="card__price">{price}</p>
+        <h3 className="card__h3">{title}</h3>
+        <p className="card__p">{description}</p>
+      </Link>
+      <Counter id={id} />
     </div>
-  )
-}
+  );
+};
 
-export default CardSm
+export default CardSm;
