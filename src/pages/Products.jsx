ce3a87b175dec4e7aa84bbe805196a266af6f4f5
“Guada” <guadalupearroferia@gmail.com>
2024-05-13 12:11:25 +1000

    ADD class to card container

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import CardsContainer from '../components/layout/CardsContainer';
 import { getProducts } from '../util/api';
+import Text from '../components/atoms/Text';
 
 const Products = () => {
   const [products, setProducts] = useState([])
@@ -16,8 +17,9 @@ const Products = () => {
   return (
     <>
     <>
-      <main>
-        <CardsContainer items={products} routePrefix={`/productInfo`}/>
+      <main className='main_products'>
+        <Text renderAs="h1" content="Nuestros productos" className="section-cards_h2"/>
+        <CardsContainer items={products} routePrefix={`/productInfo`} cardWrapperClass='card_wrapper-lg'/>
       </main>
     </>
    
