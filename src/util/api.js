3aba277821eeabf0e2c62322568775860d9857ce
“Guada” <guadalupearroferia@gmail.com>
2024-06-03 12:32:48 +1000

    DELETE console log

--- src/util/api.js
+++ src/util/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-console.log(process.env.REACT_APP_BASE_URL_API)
-
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL_API,
     headers: {
