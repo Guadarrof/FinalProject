f99d9d3ee65d72cc61d4d37d0dee6e52f42a7236
“Guada” <guadalupearroferia@gmail.com>
2024-06-07 10:07:40 +1000

    FIX alert position and style

--- src/components/molecules/Alert.jsx
+++ src/components/molecules/Alert.jsx
@@ -1,37 +1,40 @@
-import React, {useEffect, useRef} from 'react'
+import React, { useEffect, useRef } from 'react';
 
-const Alert = ({children, onClose, show}) => {
+const Alert = ({ children, onClose, show }) => {
   const alertRef = useRef();
+
   useEffect(() => {
     if (show) {
+      alertRef.current.classList.add('show');
       alertRef.current.focus();
+    } else {
+      alertRef.current.classList.remove('show');
     }
   }, [show]);
 
   const handleEscPress = (e) => {
-    if (e.key === "Escape") {
+    if (e.key === 'Escape') {
       onClose();
     }
   };
+
   const handleBlur = (e) => {
     if (!alertRef.current.contains(e.relatedTarget)) {
       onClose();
     }
   };
 
-  return show ? (
-    <div className="alert_container">
-      <div
-        className="alert_content"
-        tabIndex="0"
-        ref={alertRef}
-        onBlur={handleBlur}
-        onKeyDown={handleEscPress}
-      >
-        {children}
-      </div>
+  return (
+    <div
+      className="alert_container"
+      tabIndex="0"
+      ref={alertRef}
+      onBlur={handleBlur}
+      onKeyDown={handleEscPress}
+    >
+      {children}
     </div>
-  ) : undefined;
+  );
 };
 
 export default Alert;
---
+++ src/components/organisms/CartAlert.jsx
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect } from 'react';
+import { CartContext } from '../../context/CartContext';
+import Alert from '../molecules/Alert';
+
+const CartAlert = () => {
+  const { showAlert, setShowAlert } = useContext(CartContext);
+
+  useEffect(() => {
+    if (showAlert) {
+      const timer = setTimeout(() => {
+        setShowAlert(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [showAlert, setShowAlert]);
+
+  return (
+    <Alert show={showAlert} onClose={() => setShowAlert(false)}>
+      <p className='alert_text'>Producto Agregado</p>
+    </Alert>
+  );
+};
+
+export default CartAlert;
